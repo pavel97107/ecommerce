381a31d6b67bc611e6546299f2184073c4bf9b24
pavel97107 <pavel97107@gmail.com>
2020-12-18 14:24:14 +0300

    upgradeNavigationPanel

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,32 +1,39 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import {
-  MailOutlined,
   AppstoreOutlined,
   SettingOutlined,
+  UserAddOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 
 export default () => {
-  const [current, setCurrent] = useState("");
+  const [current, setCurrent] = useState("home");
 
-  const handleClick = () => {
-    //
+  const handleClick = (e) => {
+    setCurrent(e.key);
   };
 
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Menu.Item key="mail" icon={<MailOutlined />}>
+      <Menu.Item key="home" icon={<AppstoreOutlined />}>
         Home
       </Menu.Item>
-      <SubMenu
-        key="SubMenu"
-        icon={<SettingOutlined />}
-        title="Register"
+      <Menu.Item key="login" icon={<UserOutlined />} className="float-right">
+        Login
+      </Menu.Item>
+      <Menu.Item
+        key="register"
+        icon={<UserAddOutlined />}
+        className="float-right"
       >
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
+        Register
+      </Menu.Item>
+      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
       </SubMenu>
     </Menu>
   );
