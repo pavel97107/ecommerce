16a74d24c5ec39ded7c3fc63dfacf22a2d997722
pavel97107 <pavel97107@gmail.com>
2020-12-18 13:45:27 +0300

    createNavigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Login, Register, Home } from "./pages";
+import {Nav} from './components'
+
+//styles
+import 'antd/dist/antd.css'
+
 
 const App = () => {
   return (
     <div className="App">
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
