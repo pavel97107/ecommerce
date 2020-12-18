9bd44abdf841b4b12e48a322cf6266a9c34da552
pavel97107 <pavel97107@gmail.com>
2020-12-18 15:34:37 +0300

    createRegisterForm

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase";
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBi4B4SyTKinslyATWUXHF7WmwuG4KYyL8",
--- src/pages/auth/register/index.js
+++ src/pages/auth/register/index.js
@@ -1,10 +1,52 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { auth } from "../../../firebase";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default () => {
-    return (
-        <div>
-            <h1>Register</h1>
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      url: "http://localhost:3000/register/complete",
+      handleCodeApp: true,
+    };
+    try {
+      await auth.sendSignInLinkToEmail(email, config);
+      toast.success(
+        `Email is sent to ${email}. Click the link to complete your registration`
+      );
+      localStorage.setItem("emailForRegistration", email);
+      setEmail("");
+    } catch (e) {
+      toast.error(`Something went wrong`);
+    }
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        autoFocus
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit" className="btn btn-raised">
+        Register / {email}
+      </button>
+    </form>
+  );
+
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
