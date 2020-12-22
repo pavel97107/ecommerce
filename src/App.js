584591805db61c8fb1f4fbb957109d1a5ce73871
pavel97107 <pavel97107@gmail.com>
2020-12-22 12:47:26 +0300

    getToken

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { Login, Register, Home, RegisterComplete } from "./pages";
 import { ToastContainer } from "react-toastify";
 import { Nav } from "./components";
 
-
 //styles
 import "react-toastify/dist/ReactToastify.css";
 import "antd/dist/antd.css";
 
+//redux fn
+import { useSelector, useDispatch } from "react-redux";
+import { auth } from "./firebase";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const { token } = await user.getIdTokenResult();
+        console.log(token);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: { email: user.email, token: token },
+        });
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <Nav />
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,7 +2,7 @@ export default (state = null, action) => {
   switch (action.type) {
     case "LOGGED_IN_USER":
       return {
-        user: action.payload,
+        ...action.payload,
       };
     case "LOG_OUT":
       return null;
