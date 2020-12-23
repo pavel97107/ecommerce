3a0f007f1ca3b0af7fd06555ae3452d9f288cd0b
pavel97107 <pavel97107@gmail.com>
2020-12-23 20:54:29 +0300

    finishLoginInNodeAPI

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -9,7 +9,7 @@ import axios from "axios";
 
 const createOrUpdateUser = async (authtoken) => {
   return await axios.post(
-    `${process.env.REACT_APP_API}`,
+    `${process.env.REACT_APP_API}/create-or-update-user`,
     {},
     {
       headers: {
