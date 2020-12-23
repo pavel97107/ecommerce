2ea17b51257d925967d24a31918eb782b692f27c
pavel97107 <pavel97107@gmail.com>
2020-12-23 22:05:42 +0300

    updateController

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,18 @@
 const User = require("../models/user");
 
 exports.createOrUpdateUser = async function (req, res) {
-  console.log("MY USER", req.user);
   const { name, email, picture } = req.user;
 
-  const currentUser = await User.findOneAndUpdate(
+  const user = await User.findOneAndUpdate(
     { email },
     { name, picture },
     { new: true }
   );
 
-  if (!currentUser) {
-    const newUser = new User({ name, email, picture });
-    await newUser.save();
-    console.log("NEW USER", newUser);
-    return res.json({ newUser });
+  if (!user) {
+    const user = new User({ name, email, picture });
+    await user.save();
+    return res.json({ user });
   }
-  console.log("CURRENT USER", currentUser);
-  res.json({ currentUser });
+  res.json({ user });
 };
