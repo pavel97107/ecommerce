e0c86f6e7d198091e6f871db3f39db06611c0b9b
pavel97107 <pavel97107@gmail.com>
2020-12-28 21:39:32 +0300

    createCategoryControllers

---
+++ controllers/category.js
@@ -0,0 +1,9 @@
+exports.create = async function (req, res) {};
+
+exports.read = async function (req, res) {};
+
+exports.update = async function (req, res) {};
+
+exports.remove = async function (req, res) {};
+
+exports.list = async function (req, res) {};
---
+++ routes/category.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const router = Router();
+//controllers
+const {
+  create,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category");
+//middleware
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+router.post("/category", authCheck, adminCheck, create);
+router.get("/category", authCheck, adminCheck, list);
+router.get("/category/:slug", authCheck, adminCheck, read);
+router.put("/category/:slug", authCheck, adminCheck, update);
+router.delete("/category/:slug", authCheck, adminCheck, remove);
+
+module.exports = router;
+ 
