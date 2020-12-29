7d6aef6e0ee6c4ee1915b8540e523982f44e1dbf
pavel97107 <pavel97107@gmail.com>
2020-12-29 13:03:24 +0300

    realizeCRUDreqByCategory

--- controllers/category.js
+++ controllers/category.js
@@ -12,10 +12,47 @@ exports.create = async function (req, res) {
   }
 };
 
-exports.read = async function (req, res) {};
+exports.read = async function (req, res) {
+  try {
+    const category = await Category.findOne({ slug: req.params.slug });
+    res.json(category);
+  } catch (e) {
+    res.json({ message: e.message });
+  }
+};
 
-exports.update = async function (req, res) {};
+exports.update = async function (req, res) {
+  const { name } = req.body;
+  try {
+    const category = await Category.findOneAndUpdate(
+      {
+        slug: req.params.slug,
+      },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(category);
+  } catch (e) {
+    res.json({ message: e.message });
+  }
+};
 
-exports.remove = async function (req, res) {};
+exports.remove = async function (req, res) {
+  try {
+    const deleted = await Category.findOneAndDelete({
+      slug: req.params.slug,
+    });
+    res.json(deleted);
+  } catch (e) {
+    res.json({ message: e.message });
+  }
+};
 
-exports.list = async function (req, res) {};
+exports.list = async function (req, res) {
+  try {
+    const categories = await Category.find({}).sort({ createdAt: -1 });
+    res.json(categories);
+  } catch (e) {
+    res.json({ message: e.message });
+  }
+};
