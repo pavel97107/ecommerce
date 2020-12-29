655b557b2219fd183972628ef3b9ba7633a7ff88
pavel97107 <pavel97107@gmail.com>
2020-12-29 12:37:27 +0300

    realizeCreateControllerForCategory

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,16 @@
-exports.create = async function (req, res) {};
+const Category = require("../models/category");
+const slugify = require("slugify");
+
+exports.create = async function (req, res) {
+  try {
+    const { name } = req.body;
+    const category = new Category({ name, slug: slugify(name) });
+    await category.save();
+    res.json(category);
+  } catch (e) {
+    res.status(400).json({ message: "Create category failed" });
+  }
+};
 
 exports.read = async function (req, res) {};
 
